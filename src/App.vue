cf9d4d5f93cf7798e893dfefce18561a169befce
jacklincao <1287075856@qq.com>
2018-03-16 10:59:29 +0800

    添加sass

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="ddd">123</div>
     <router-view/>
   </div>
 </template>
@@ -10,7 +11,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,5 +19,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .ddd {
+    color: blue;
+  }
 }
 </style>
