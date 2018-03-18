993d01a4787075ef80e2bc1ce1862257c2b6cd8f
e <846022604@qq.com>
2018-03-18 21:54:10 +0800

    修改login组件

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,40 +1,44 @@
 <template>
-<<<<<<< HEAD
   <div class="login">
     <el-container>
       <el-header class="header"></el-header>
       <el-main class="main">
         <div class="loginBox el-col-lg-6 el-col-md-offset-14">
-          <div class="head"></div>
-          <div class="name">
+          <el-row class="head"></el-row>
+          <el-row class="name">
             <h3>框架系统登录</h3>
-          </div>
-          <from>
-            <div>
-              <div>
-                <div></div>
-                <div></div>
-                <button></button>
-              </div>
-              <div></div>
-            </div>
-          </from>
+          </el-row>
+          <el-form ref="user" :model="user" label-width="80px">
+            <el-from-item >
+              <el-input v-model="user.name" placeholder="用户名" size="medium" style="padding-bottom: 5px;">
+                <el-col slot="prefix" class="loginIcon icon-user"></el-col>
+              </el-input>
+            </el-from-item>
+            <el-from-item>
+              <el-input type="password" v-model="user.password" placeholder="密码" size="medium"  style="padding-bottom: 5px;">
+                <el-col slot="prefix" class="loginIcon icon-password"></el-col>
+              </el-input>
+            </el-from-item>
+            <el-row style="padding:7px 0 7px 0">
+              <el-from-item>
+                <el-checkbox label="记住密码" name="type"></el-checkbox>
+                <a class="forget_password">忘记密码</a>
+              </el-from-item>
+            </el-row>
+            <el-form-item>
+              <el-button type="primary" @click="loginIn" style="width:100%;font-size: 18px;">登录</el-button>
+            </el-form-item>
+          </el-form>
         </div>
       </el-main>
     </el-container>
-=======
-  <div>
-    <label for="">用户名</label><input v-model="person.operatorCode"/>
-    <label for="">密码</label><input v-model="person.operatorPassword"/>
-    <button @click="loginIn">登录</button>
->>>>>>> 400f92e049eb42044a917f7b5f50db93be6a6e4a
   </div>
 </template>
 <script>
 export default {
   data: function () {
     return {
-      person: {
+      user: {
         name: '',
         password: ''
       }
@@ -51,6 +55,15 @@ export default {
   }
 }
 </script>
+<style>
+  .login .el-input__inner{
+    padding-left: 36px;
+  }
+  .login .el-form-item__content{
+    margin-left: 0 !important;
+  }
+</style>
+
 <style lang="scss" scoped>
   body{
     margin:0;
@@ -59,6 +72,14 @@ export default {
   .login{
     width:100%;
     height: 100%;
+    .loginIcon{
+      background-image: url("../../assets/images/loginImg/icon.png");
+      width: 36px;
+      height: 36px;
+      position: absolute;
+      z-index: 2;
+    }
+
     .header{
       background:url("../../assets/images/loginImg/loginHead.png") no-repeat;
       width: 100%;
@@ -98,6 +119,44 @@ export default {
           margin-right: -15px;
           margin-left: -15px;
         }
+        .el-input__inner{
+          padding-left:36px;
+        }
+        .icon-user{
+          background-position: -5px -10px;
+        }
+        .icon-password{
+          background-position: -5px -55px;
+        }
+        .forget_password{
+          cursor: pointer;
+          text-decoration: none;
+          float: right;
+          font-size: 14px;
+          &:hover{
+            color: #000;
+          }
+        }
+        .input-group{
+          width: 100%;
+          padding-bottom: 5px;
+          background-color: white;
+          position: relative;
+          display: table;
+          border-collapse: separate;
+          /*.icon-user{
+           position: absolute;
+           z-index: 2;
+           background-position: -5px -10px;
+          }*/
+          /*input{
+           padding-left: 36px;
+           color: #ffffff;
+            border-radius: 5px;
+            border: none;
+          }*/
+        }
+
         .name{
           text-align: center;
           margin-right: -15px;
